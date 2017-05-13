37e438d977e05459213cf2be047309562b9a7635
Pete Zybrick <pzybrick@gmail.com>
2017-05-13 19:34:25 -0400

    pills

--- iote2e-pyclient/src/iote2epyclient/test/testprocesspilldispenser.py
+++ iote2e-pyclient/src/iote2epyclient/test/testprocesspilldispenser.py
@@ -1,14 +1,44 @@
 import time
 from iote2epyclient.pilldispenser.handlepilldispenser import HandlePillDispenser
+import base64
+from io import BytesIO
+from picamera import PiCamera
+
 
 def main():
     handlePillDispenser = HandlePillDispenser()
-    testDispensePills(handlePillDispenser)
+    imageStream = BytesIO()
+    camera = PiCamera(resolution=(100,100))
+    camera.contrast = 0
+    camera.sharpness = 100
+    camera.capture( imageStream, 'png' )
+    
+    dispensePills(handlePillDispenser)
+    takePicture(camera, imageStream)
     print('done')
 
-def testDispensePills(handlePillDispenser):
+def dispensePills(handlePillDispenser):
     print('dispensing 4 pills')
     handlePillDispenser.dispensePills(4)
+    
+def takePicture(camera, imageStream):
+    print('Taking picture')
+    print('before encode')
+    print( 'len=' + str( len(imageStream.getvalue() ) ))
+    
+    encoded = base64.b64encode(imageStream.getvalue() )
+    #print("Encoded=" + encoded )
+    print("Encoded len=" + str( len(encoded)))
+    imageStream.close()
+    
+    print('after encode')
+    
+    decoded = base64.b64decode(encoded)
+    print("Decoded=" + decoded )
+    print("Decoded=" +  str( len(decoded)))
+    
+    with open('/tmp/iote2e-test.png', 'wb') as output:
+        output.write(decoded)
 
 if __name__ == '__main__':
     main()
