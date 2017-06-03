4d84d666c77bac6d3740a980561641e1954e7e94
Pete Zybrick <pzybrick@gmail.com>
2017-06-03 11:23:37 -0400

    testing

--- iote2e-pyclient/src/iote2epyclient/test/testprocesspilldispenser.py
+++ iote2e-pyclient/src/iote2epyclient/test/testprocesspilldispenser.py
@@ -1,5 +1,6 @@
 import time
 from iote2epyclient.pilldispenser.handlepilldispenser import HandlePillDispenser
+from iote2epyclient.pilldispenser.blinkledthread import BlinkLedThread
 import base64
 from io import BytesIO
 from picamera import PiCamera
@@ -7,13 +8,17 @@ import piplates.DAQCplate as DAQC
 
 
 def main():
+    blinkLedThread = BlinkLedThread()
+    blinkLedThread.start()
+    time.sleep(5)
+    blinkLedThread.shutdown()
+    
     handlePillDispenser = HandlePillDispenser()
     imageStream = BytesIO()
-    camera = PiCamera(resolution=(100,100))
+    camera = PiCamera(resolution=(200,200))
     camera.contrast = 0
     camera.sharpness = 100
-    camera.capture( imageStream, 'png' )
-    
+   
     dispensePills(handlePillDispenser)
     takePicture(camera, imageStream)
     leds()
@@ -35,14 +40,18 @@ def leds():
         time.sleep(.25)
         if DAQC.getINTflags(0) == 256:
             break
+    for i in range(0,100):
+        DAQC.clrLED(0,0)
+        DAQC.clrLED(0,1)
 
 
 def dispensePills(handlePillDispenser):
-    print('dispensing 1 pills')
-    handlePillDispenser.dispensePills(1)
+    print('dispensing 3 pills')
+    handlePillDispenser.dispensePills(3)
     
 def takePicture(camera, imageStream):
     print('Taking picture')
+    camera.capture( imageStream, 'png' )
     print('before encode')
     print( 'len=' + str( len(imageStream.getvalue() ) ))
     
@@ -59,6 +68,7 @@ def takePicture(camera, imageStream):
     
     with open('/tmp/iote2e-test.png', 'wb') as output:
         output.write(decoded)
+        output.close()
 
 if __name__ == '__main__':
     main()
